1166f388b82c1a022bfcefe53881d7241c4e2f3f
Exorbis <Exorbis@users.noreply.github.com>
2016-06-15 15:58:04 -0400

    Update CenterPanel.java

--- src/initialpkg/CenterPanel.java
+++ src/initialpkg/CenterPanel.java
@@ -6,22 +6,33 @@ import java.util.ArrayList;
 import processing.core.*;
 
 
+/**
+ * This panel is the CenterPanel (read: center panel) for the StormView client GUI. It operates as the a display that holds circles of all the states (uniquely) present in the storm data
+ * Within each circle contains the amount of times there has been a storm in that state/territory/zone of any kind 
+ * It also acts as the super for LowerPanel in order to pass its primary method as well as the PApplet functionality
+ * 
+ * @author Marc Pelve
+ * @since JDK 8
+ * @since June 15, 2016
+ * @version 1.1
+ *
+ */
 public class CenterPanel extends PApplet {
 	
 	float red, green, blue;
 	double intensity;
 
 	
-	static String input, secondaryInput;
 	ArrayList<String> timezones = StormView.returnNonRepeats(StormController.getSortedTimezone(), "timezone");
 	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 	int paneWidth = (int) screenSize.getWidth();
 	int paneHeight = (int) screenSize.getHeight();
-	
-	double amountOfStorms;
-	int stateMax;
+
 	ArrayList<String> uniqueState = StormView.returnNonRepeats(StormController.getSortedState(), "state");
 
+	/**
+	 * This is the initialization on first running of the sketch, which includes the size of the panel and background color
+	 */
 	public void setup(){
 		
 		size((int) (screenSize.getWidth() * 0.9), (int) (screenSize.getHeight() * 0.8));
@@ -32,7 +43,10 @@ public class CenterPanel extends PApplet {
 	}
 	
 	
-	
+	/**
+	 * This method allows for the ongoing illustration of the circles and other provided visuals
+	 * This holds the center "state" circles and the time zones that are present in the file data
+	 */
 	public void draw(){
 	
 		
@@ -72,6 +86,12 @@ public class CenterPanel extends PApplet {
 		
 	}
 	
+	/**
+	 * This method takes in a decimal percentage in order to create a color gradient based on how intense (1 being the most -> the max -> and 0 being the least)
+	 * A shade of red is set on the high end, and a shade of green is set on the low end
+	 * 
+	 * @param intensityValue is the decimal percentage of the inputed value that is used to create a gradient based on the value
+	 */
 	void setColor(double intensityValue){
 		blue = (float) 0;
 		if (0 <= intensityValue && intensityValue < 0.5){
@@ -84,14 +104,7 @@ public class CenterPanel extends PApplet {
 		}
 		
 	}
-	
-	static void setInput(String inputChange){
-		input = inputChange.toLowerCase();
-	}
-	
-	static void setInput2(String inputChange){
-		secondaryInput = inputChange.toLowerCase();
-	}
+
 
 	
 }
