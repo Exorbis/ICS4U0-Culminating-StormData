c7aeff7879460a43df84cbedfdccf06162279937
Exorbis <Exorbis@users.noreply.github.com>
2016-06-15 15:58:27 -0400

    Update LowerPanel.java

--- src/initialpkg/LowerPanel.java
+++ src/initialpkg/LowerPanel.java
@@ -4,6 +4,19 @@ import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.util.Collections;
 
+
+/**
+ * This panel is the bottom of the StormView client which displays the circles based off the total damages in the month (color) and the 
+ * severity of the magnitude of the storm (size) 
+ * LowerPanel extends CenterPanel as it inherits the properties from super including the PApplet attribute
+ * 
+ * @author Marc Pelve
+ * @since JDK 8
+ * @since June 15, 2016
+ * @version 1.1
+ *
+ */
+//This uses integrated processing features from parent 
 public class LowerPanel extends CenterPanel {
 
 	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
@@ -13,7 +26,10 @@ public class LowerPanel extends CenterPanel {
 	double intensity, magIntensity;
 
 
-	
+	/**
+	 * This is the default constructor that is initialized when running the StormView client
+	 * It displays the lower panel containing the circles based off color and size dependent on the data
+	 */
 	public void setup(){
 		
 		size((int) (screenSize.getWidth() * 0.9), (int) ((screenSize.getHeight() * 0.8)*0.2));
@@ -47,12 +63,13 @@ public class LowerPanel extends CenterPanel {
 
 
 
-	public void draw(){
-
-		
-		
-	}
 	
+	/**
+	 * This method takes in a decimal percentage in order to create a color gradient based on how intense (1 being the most -> the max -> and 0 being the least)
+	 * A shade of red is set on the high end, and a shade of green is set on the low end
+	 * 
+	 * @param intensityValue is the decimal percentage of the inputed value that is used to create a gradient based on the value
+	 */
 	void setColor(double intensityValue){
 		blue = (float) 0;
 		if (0 <= intensityValue && intensityValue < 0.5){
