5ad557d5df49e690d440e17643cb7e46069661a2
Exorbis <Exorbis@users.noreply.github.com>
2016-06-15 14:43:23 -0400

    Update CenterPanel.java

--- src/initialpkg/CenterPanel.java
+++ src/initialpkg/CenterPanel.java
@@ -3,19 +3,14 @@ package initialpkg;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.util.ArrayList;
-import java.util.Collections;
-
 import processing.core.*;
 
 
 public class CenterPanel extends PApplet {
 	
 	float red, green, blue;
-	double max;
 	double intensity;
-	int ellipseCordY = 100;
-	int direction = 1;
-	int speed = 2;
+
 	
 	static String input, secondaryInput;
 	ArrayList<String> timezones = StormView.returnNonRepeats(StormController.getSortedTimezone(), "timezone");
@@ -25,16 +20,14 @@ public class CenterPanel extends PApplet {
 	
 	double amountOfStorms;
 	int stateMax;
+	ArrayList<String> uniqueState = StormView.returnNonRepeats(StormController.getSortedState(), "state");
 
 	public void setup(){
 		
 		size((int) (screenSize.getWidth() * 0.9), (int) (screenSize.getHeight() * 0.8));
 		background(255);
 		
-		stateMax = Collections.max(StormController.getTotalStormsPerState());
 		
-		for (int i = 0; i < StormController.getTotalStormsPerState().size(); i++){
-		}
 		
 	}
 	
@@ -46,14 +39,24 @@ public class CenterPanel extends PApplet {
 		background(255);
 		fill(200);
 		ellipseMode(CENTER);
+		textAlign(CENTER);
 		ArrayList<String> uniqueState = StormView.returnNonRepeats(StormController.getSortedState(), "state");
 		double interval = (screenSize.getWidth() * 0.9)/timezones.size();
-		double intervalEllipse = (screenSize.getWidth() * 0.9)/uniqueState.size();
-		int xcordEllipse = (int) (intervalEllipse/2);
+		double intervalEllipse = (screenSize.getWidth() * 0.9)/(uniqueState.size()/4);
+		int xcordEllipse = (int) (intervalEllipse) - 50;
+		int ycordEllipse = 100;
 		
-		for (int i = 0; i < uniqueState.size(); i++){
-			ellipse(xcordEllipse, height/2, 25, 25);
+		for (int i = 0; i <uniqueState.size(); i++){
+			fill(200);
+			ellipse(xcordEllipse, ycordEllipse, 50, 50);
+			text((uniqueState.get(i).substring(0, 4)), xcordEllipse, ycordEllipse - 30);
+			fill(255);
+			text(StormController.getTotalStormsPerState().get(i), xcordEllipse, ycordEllipse+2);
 			xcordEllipse += intervalEllipse;
+			if (xcordEllipse > (intervalEllipse * (uniqueState.size() / 4))){
+				xcordEllipse = (int) (intervalEllipse) - 50;
+				ycordEllipse += intervalEllipse;
+			}
 		}
 		
 		
